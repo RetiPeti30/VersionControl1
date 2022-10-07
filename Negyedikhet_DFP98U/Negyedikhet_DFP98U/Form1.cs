ff2f7362eb8bd5395e28035c174d935ce4eb3953
dfp98u <dfp98u@SZA0025792>
2022-10-07 12:30:29 +0200

    Hat

--- Negyedikhet_DFP98U/Negyedikhet_DFP98U/Form1.cs
+++ Negyedikhet_DFP98U/Negyedikhet_DFP98U/Form1.cs
@@ -17,15 +17,45 @@ namespace Negyedikhet_DFP98U
     {
         RealEstateEntities real = new RealEstateEntities();
         List<Flat> Flatties;
+
+        Excel.Application xlAPP;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlShit;
         public Form1()
         {
             InitializeComponent();
             LoadData();
+            CreateExcel();
         }
 
         private void LoadData()
         {
             Flatties = real.Flats.ToList();
+        }
+
+        private void CreateExcel()
+        {
+            try
+            {
+                xlAPP = new Excel.Application();
+                xlWB = xlAPP.Workbooks.Add(Missing.Value);
+                xlShit = xlWB.ActiveSheet();
+                //CreateTable();
+                xlAPP.Visible = true;
+                xlAPP.UserControl = true;
+
+            }
+            catch ( Exception ex)
+            {
+                string errMsg = string.Format("Error: {0}\nLine {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlAPP.Quit();
+                xlWB = null;
+                xlAPP = null;
+
+            }
         }
     }
 }
