adc4203810478eaf4ed319a6ed784a6010a9c30e
dfp98u <dfp98u@SZA0025792>
2022-11-04 12:12:16 +0100

    Labda creation

---
+++ Mikulas/Mikulas/Entities/Ball.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mikulas.Entities
+{
+    class Ball : Label
+    {
+        public Ball()
+        {
+            AutoSize = false;
+            Width = 50;
+            Height = Width;
+            Paint += Ball_Paint;
+        }
+
+        private void Ball_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+        }
+
+        protected void DrawImage(Graphics graphics)
+        {
+            graphics.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
+        }
+
+        public void MoveBall()
+        {
+            Left += 1;
+        }
+    }
+}
