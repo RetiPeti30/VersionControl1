bece706fec0cd4afe61acb57e5a87bb4f11801df
dfp98u <dfp98u@SZA0025792>
2022-11-04 12:14:30 +0100

    Labdagyar

---
+++ Mikulas/Mikulas/Entities/BallFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mikulas.Entities
+{
+    class BallFactory
+    {
+        public Ball CreateNew()
+        {
+            return new Ball();
+        }
+    }
+}
