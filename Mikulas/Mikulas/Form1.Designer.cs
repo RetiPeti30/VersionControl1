b0e0cbebbfbc3e1f2f503f28f86825e93e6622d8
noi121 <noemi.gazdik@stud.uni-corvinus.hu>
2022-11-04 23:53:51 +0100

    Gombszin

--- Mikulas/Mikulas/Form1.Designer.cs
+++ Mikulas/Mikulas/Form1.Designer.cs
@@ -36,6 +36,7 @@ namespace Mikulas
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // panel1
@@ -70,9 +71,9 @@ namespace Mikulas
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(432, 47);
+            this.button2.Location = new System.Drawing.Point(353, 48);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(260, 85);
+            this.button2.Size = new System.Drawing.Size(207, 85);
             this.button2.TabIndex = 2;
             this.button2.Text = "Car";
             this.button2.UseVisualStyleBackColor = true;
@@ -87,11 +88,23 @@ namespace Mikulas
             this.label1.TabIndex = 3;
             this.label1.Text = "Coming Next:";
             // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button3.Location = new System.Drawing.Point(597, 44);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(155, 88);
+            this.button3.TabIndex = 4;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -112,6 +125,7 @@ namespace Mikulas
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Mikulas/Mikulas/Form1.cs
+++ Mikulas/Mikulas/Form1.cs
@@ -79,5 +79,16 @@ namespace Mikulas
             _nextToy.Left = label1.Left;
             Controls.Add(_nextToy);
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            var colorPicker = new ColorDialog();
+
+            colorPicker.Color = button.BackColor;
+            if (colorPicker.ShowDialog() != DialogResult.OK)
+                return;
+            button.BackColor = colorPicker.Color;
+        }
     }
 }
