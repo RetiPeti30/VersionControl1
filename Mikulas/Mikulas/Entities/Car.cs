a799d7b82ed7b1c2628151f4373d9966f396ecaf
noi121 <noemi.gazdik@stud.uni-corvinus.hu>
2022-11-04 23:20:16 +0100

    Gyar

---
+++ Mikulas/Mikulas/Entities/Car.cs
@@ -0,0 +1,19 @@
+﻿using Mikulas.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mikulas.Entities
+{
+    class Car : Toy
+    {
+        protected override void DrawImage(Graphics graphics)
+        {
+            Image imageFile = Image.FromFile("Images/car.png");
+            graphics.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+        }
+    }
+}
--- Mikulas/Mikulas/Form1.cs
+++ Mikulas/Mikulas/Form1.cs
@@ -41,7 +41,7 @@ namespace Mikulas
             var maxPosition = 0;
             foreach (var ball in _balls)
             {
-                ball.MoveBall();
+                ball.MoveToy();
                 if (ball.Left > maxPosition)
                     maxPosition = ball.Left;
             }
