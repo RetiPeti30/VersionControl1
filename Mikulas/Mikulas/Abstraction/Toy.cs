eb36f9b64deafe15d46542d6b51ca767f2209891
dfp98u <dfp98u@SZA0025792>
2022-11-04 12:38:26 +0100

    Jatekabsztrakcio

---
+++ Mikulas/Mikulas/Abstraction/Toy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mikulas.Abstraction
+{
+    public abstract class Toy : Label
+    {
+        public Toy()
+        {
+            AutoSize = false;
+            Width = 50;
+            Height = Width;
+            Paint += Toy_Paint;
+        }
+
+        private void Toy_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+        }
+
+        protected abstract void DrawImage(Graphics graphics);
+        
+
+        public void MoveToy()
+        {
+            Left += 1;
+        }
+    }
+}
