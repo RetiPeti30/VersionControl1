742635cdd19f4324efd814fb441fb7f4a481bb93
dfp98u <dfp98u@SZA0025792>
2022-11-04 12:40:19 +0100

    labdaabsztrakt

--- Mikulas/Mikulas/Entities/Ball.cs
+++ Mikulas/Mikulas/Entities/Ball.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Mikulas.Abstraction;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -8,29 +9,15 @@ using System.Windows.Forms;
 
 namespace Mikulas.Entities
 {
-    public class Ball : Label
+    public class Ball : Toy
     {
-        public Ball()
-        {
-            AutoSize = false;
-            Width = 50;
-            Height = Width;
-            Paint += Ball_Paint;
-        }
+       
 
-        private void Ball_Paint(object sender, PaintEventArgs e)
-        {
-            DrawImage(e.Graphics);
-        }
-
-        protected void DrawImage(Graphics graphics)
+        protected override void DrawImage(Graphics graphics)
         {
             graphics.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
         }
 
-        public void MoveBall()
-        {
-            Left += 1;
-        }
+        
     }
 }
