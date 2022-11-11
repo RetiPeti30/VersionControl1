a1de20c06a39e0e132ca064831d7040ba5907f9b
dfp98u <dfp98u@SZA0025792>
2022-11-11 12:16:16 +0100

    Hat Hét

--- Hetedik/Hetedik/Form1.cs
+++ Hetedik/Hetedik/Form1.cs
@@ -30,5 +30,20 @@ namespace Hetedik
 
             dataGridView2.DataSource = Portfolio;
         }
+        
+        private decimal GetPortfolioValue(DateTime date)
+        {
+            decimal value = 0;
+            foreach (var item in Portfolio)
+            {
+                var last = (from x in Ticks
+                            where item.Index == x.Index.Trim()
+                               && date <= x.TradingDay
+                            select x)
+                            .First();
+                value += (decimal)last.Price * item.Volume;
+            }
+            return value;
+        }
     }
 }
