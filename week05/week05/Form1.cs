e04441f441b7908dbd24ac7834a88aa225e76a2b
noi121 <noemi.gazdik@stud.uni-corvinus.hu>
2022-11-16 10:53:19 +0100

    otodik

--- week05/week05/Form1.cs
+++ week05/week05/Form1.cs
@@ -7,15 +7,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
+using week05.Entities;
 using week05.ServiceReference1;
 
 namespace week05
 {
     public partial class Form1 : Form
     {
+        BindingList<RateData> Rates = new BindingList<RateData>();
         public Form1()
         {
             InitializeComponent();
+            dataGridView1.DataSource = Rates.ToList();
+            string x = GetExchange();
+
         }
 
         private string GetExchange()
@@ -34,6 +40,30 @@ namespace week05
 
             return result;
 
+        }
+
+        private void GetXml(string result)
+        {
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                var rate = new RateData();
+                Rates.Add(rate);
+
+                rate.Date = DateTime.Parse(element.GetAttribute("data"));
+
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                {
+                    rate.Value = value / unit;
+                }
+            }
         }
     }
 }
