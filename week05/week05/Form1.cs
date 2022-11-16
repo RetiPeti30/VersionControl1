ed1efcf6f8e58482e6228b1c1f7b9db1b4073f05
noi121 <noemi.gazdik@stud.uni-corvinus.hu>
2022-11-16 10:41:05 +0100

    harmadik

--- week05/week05/Form1.cs
+++ week05/week05/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week05.ServiceReference1;
 
 namespace week05
 {
@@ -15,6 +16,24 @@ namespace week05
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private string GetExchange()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
+
+            return result;
+
         }
     }
 }
