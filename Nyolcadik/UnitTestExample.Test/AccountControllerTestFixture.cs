273536f1353b7e77dc0587db255d932aff96bb75
dfp98u <dfp98u@SZA0025792>
2022-11-25 12:53:47 +0100

    email validate fv

---
+++ Nyolcadik/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestExample.Test
+{
+    public class AccountControllerTestFixture
+    {
+        [Test]
+        public void TestValidateEmail(string email, bool expectedResult)
+        {
+
+        }
+    }
+
+    
+}
