d34a810c95f3a8baac849aa62ab91843d6d5e0a9
RetiPeti30 <peter.reti@stud.uni-corvinus.hu>
2022-11-25 23:57:28 +0100

    fos

---
+++ UnitTestExample/Controllers/AccountController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using UnitTestExample.Abstractions;
+using UnitTestExample.Entities;
+using UnitTestExample.Services;
+
+namespace UnitTestExample.Controllers
+{
+    public class AccountController
+    {        
+        public IAccountManager AccountManager { get; set; }
+
+        public AccountController()
+        {
+            AccountManager = new AccountManager();
+        }
+
+        public Account Register(string email, string password)
+        {
+            if(!ValidateEmail(email))
+                throw new ValidationException(
+                    "A megadott e-mail cím nem megfelelő!");
+            if(!ValidatePassword(password))
+                throw new ValidationException(
+                    "A megadottt jelszó nem megfelelő!\n" +
+                    "A jelszó legalább 8 karakter hosszú kell legyen, csak az angol ABC betűiből és számokból állhat, és tartalmaznia kell legalább egy kisbetűt, egy nagybetűt és egy számot.");
+
+            var account = new Account()
+            {
+                Email = email,
+                Password = password,
+            };
+
+            var newAccount = AccountManager.CreateAccount(account);
+
+            return newAccount;
+        }
+
+        public bool ValidateEmail(string email)
+        {            
+            return Regex.IsMatch(
+                email, 
+                @"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?");
+        }
+
+        public bool ValidatePassword(string password)
+        {
+            return Regex.IsMatch(
+                password,
+                @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$");
+        }
+    }
+}
